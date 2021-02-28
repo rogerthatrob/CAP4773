bf13c4de34cf3b8e8028a435ed444b4a2c435a3a
Robert Haire <rogerthatrob@gmail.com>
2021-02-28 11:51:38 -0500

    created a pure r code file

---
+++ r_code_test.R
@@ -0,0 +1,15 @@
+
+
+library(ISLR)
+
+College
+summary(College)
+
+maxF_UnderGrad = max(College$F.Undergrad)
+maxP_UnderGrad = max(College$P.Undergrad)
+maxUnderGrad = maxF_UnderGrad + maxP_UnderGrad
+maxUnderGrad
+
+library(ggplot2)
+
+ggplot(data = College) + geom_point(mapping = aes(x = Top10perc, y = Outstate)) + geom_smooth(mapping = aes(x = Top10perc, y = Outstate), level = 0.95)
